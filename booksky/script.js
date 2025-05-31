bca9a521b5497478bfb6f1537a4219f8736b44ee
Libin2004 <libinlibinlibin10@gmail.com>
2025-05-31 16:51:47 +0530

    finished

---
+++ booksky/script.js
@@ -0,0 +1,42 @@
+//selecting popupbox and popup overlay button
+
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popupbox")
+
+var addpopupbutton=document.getElementById("add-popup-button")
+
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+
+//select cancel button
+
+var cancelpopup=document.getElementById("cancel")
+cancelpopup.addEventListener("click",function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+
+//select container,booktitle,addform,bookauthor,bookdescription
+
+var container=document.querySelector(".container")
+var addbook=document.getElementById("add-book")
+var booktitle=document.getElementById("book-title-input")
+var bookauthor=document.getElementById("book-author-input")
+var bookdescription=document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitle.value}</h2><h5>${bookauthor.value}</h5><p>${bookdescription.value}</p><button onclick="deletebook(event)">delete</button>`
+    container.append(div)
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+
+function deletebook(event){
+    event.target.parentElement.remove()
+}
